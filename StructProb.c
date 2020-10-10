f57b04b2b985d253be1f5fe294464867d40097f9
Emily <eeweather@gmail.com>
2020-10-09 18:44:00 -0700

    rain

--- StructProb.c
+++ StructProb.c
@@ -5,3 +5,36 @@ void main(void)
     printf("hello");
     return;
 }
+
+    color_t makeColor(int red, int green, int blue){
+
+        color_t new;
+
+        if (red>255){
+            new.red=255;
+        }
+        else if (red<0){
+            new.red=0;
+        }
+        else {
+            new.red=red;
+        }
+
+        if (green>255){
+            new.green=255;
+        }
+        else if (green<0){
+            new.green=0;
+        }
+        else{
+            new.green=green;
+        }
+        if (blue>255){
+            new.blue=255;
+        }
+        else if (blue<0){
+            new.blue=0;
+        }
+        
+        return new;
+    }
